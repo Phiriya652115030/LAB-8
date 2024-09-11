3ec32878f95bef7b996ca08c5b2a18ed91edf319
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2024-09-11 21:54:34 +0700

    4.6

--- src/main/java/se331/lab/service/EventServiceImpl.java
+++ src/main/java/se331/lab/service/EventServiceImpl.java
@@ -1,6 +1,7 @@
 package se331.lab.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import se331.lab.dao.EventDao;
 import se331.lab.entity.Event;
@@ -16,7 +17,7 @@ public class EventServiceImpl implements EventService {
         return eventDao.getEventSize();
     }
     @Override
-    public List<Event> getEvents(Integer pageSize, Integer page) {
+    public Page<Event> getEvents(Integer pageSize, Integer page) {
         return eventDao.getEvents(pageSize, page);
     }
 
